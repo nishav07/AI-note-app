625c58fa72df9fbdca552584acf40304379d0a01
unknown <nishavkumar07@gmail.com>
2025-08-21 17:57:00 +0530

    config db

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const sql = require("mysql2/promise");
+
+
+const connection = sql.createPool({
+    host:process.env.HOST,
+    port:process.env.PORT,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DB
+})
+
+
+async function test() {
+    const [rows] = await connection.query("SELECT 2+2 AS result");
+    console.log(rows);
+}
+test();

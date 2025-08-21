87c97bfc5b346bb24cfdef4d805da2f2b20b56e2
unknown <nishavkumar07@gmail.com>
2025-08-21 19:35:33 +0530

    making routes and understanding workflow

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,17 @@ function send(req,res){
     res.send("aree bhai hello world")
 }
 
+function home(req,res){
+    res.render("home.ejs");
+}
+
+function notesData(req,res){
+    const {title,content} = req.body;
+    console.log(req.body)
+    res.render("")
+}
 
 module.exports = {
-    send
+    send,
+    notesData
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { send } = require("../controllers/controller")
+const { home } = require("../controllers/controller")
 
 router.get("/" ,send)
-
+router.get("/",)
+router.post("/",)
 module.exports = router;
 
 

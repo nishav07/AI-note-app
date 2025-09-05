b3e234195c43534f411e2803d7bd4d06bc87f2b4
unknown <nishavkumar07@gmail.com>
2025-09-05 20:49:16 +0530

    maked public folder for js and css

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({ extended: true }));
 
+app.set("public")
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname , "public")));
 
 const port = 6969;
 const {pool} = require("./config/db")

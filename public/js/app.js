b6050ad68f21d19d2ba45c049c400872d1a2f24e
unknown <nishavkumar07@gmail.com>
2025-09-08 19:37:32 +0530

    before using fetch for sending data for backend

--- public/js/app.js
+++
@@ -1,1 +0,0 @@
-console.log("hello wrolddddddddddddddddddddd from public folder")
---
+++ public/js/newNotes.js
@@ -0,0 +1,10 @@
+const publishBtn = document.getElementById("publishbtn");
+const draftBtn = document.getElementById("draftbtn");
+
+publishBtn.addEventListener("click" , () => {
+    console.log("publish button clicked")
+})
+
+draftBtn.addEventListener("click" , () => {
+    console.log("draft button clicked");
+})

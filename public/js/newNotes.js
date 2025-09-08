9bb03175ddb5b3a7469a02e5f2268ea4ac7dfbf8
unknown <nishavkumar07@gmail.com>
2025-09-08 20:10:14 +0530

    making custom vaidation using js

--- public/js/newNotes.js
+++ public/js/newNotes.js
@@ -1,8 +1,17 @@
 const publishBtn = document.getElementById("publishbtn");
 const draftBtn = document.getElementById("draftbtn");
+const myform = document.getElementById("myForm");
 
 publishBtn.addEventListener("click" , () => {
-    console.log("publish button clicked")
+    let validity = myform.checkValidity();
+    console.log(validity);
+    if(!validity){
+        myform.reportValidity();
+        return
+    }
+    let form = new FormData(myform);
+    console.log(Object.fromEntries(form.entries()));
+    console.log("publish button clicked");
 })
 
 draftBtn.addEventListener("click" , () => {

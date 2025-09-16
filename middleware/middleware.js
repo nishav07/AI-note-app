bf710aaac57b9365d003688cc6e065113e8eb8d9
unknown <nishavkumar07@gmail.com>
2025-09-16 16:24:54 +0530

    making middleware for auth

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,6 +4,15 @@ function hello(req,res,next){
     next();
 }
 
+function checklog(req,res,next){
+    if(req.session && req.session.user){
+        next()
+        
+    } else {
+        res.render("login.ejs")
+    }
+}
+
 function noob(req,res,next){
     console.log("this is a particular middlware");
     next();

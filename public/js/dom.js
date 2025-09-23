34896b79c5a8f62aafe2a93fc073344888080a05
unknown <nishavkumar07@gmail.com>
2025-09-23 16:05:08 +0530

    flash working well

---
+++ public/js/dom.js
@@ -0,0 +1,10 @@
+const successF = document.getElementById("flash1")
+const errorF = document.getElementById("flash2")
+
+setTimeout(() => {
+    successF.style.display = "none";
+},4000)
+
+setTimeout(() => {
+    errorF.style.display = "none";
+},4000)
